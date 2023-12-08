636cb9053dc057004c72edd044c3fc1fc67b5d91
Rufuous <122505701+aeiouhmed@users.noreply.github.com>
2023-12-08 10:18:24 +0800

    First Commit

---
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author Ahmed_Ibrahim_Adem_Hamed_S2173079
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
