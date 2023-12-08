080f15ba7d4612ecae02fe1cfb7f21cb5fd47aed
Ahmed <122505701+aeiouhmed@users.noreply.github.com>
2023-12-08 10:30:28 +0800

    Third commit

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -17,6 +17,8 @@ public class JavaApplication20 {
         System.out.println("Hello world");
         
         System.out.println("how do you do");
+
+        System.out.println("Im doing great");
     }
     
 }
